8cf5857bf42ee38a0e3955118ae192267c4eed38
hibax <julienflb.iie@gmail.com>
2017-11-01 01:09:48 +0100

    add Debug log printer

---
+++ src/Writer.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream>
+#include "Writer.h"
+
+void Writer::debug(std::string log) {
+    std::cerr << log << std::endl;
+}
---
+++ src/Writer.h
@@ -0,0 +1,17 @@
+
+#ifndef GHOSTINTHECELL_WRITER_H
+#define GHOSTINTHECELL_WRITER_H
+
+
+class Writer {
+public:
+
+    void debug(std::string log);
+
+private:
+    Writer() = default;
+};
+
+
+
+#endif //GHOSTINTHECELL_WRITER_H
